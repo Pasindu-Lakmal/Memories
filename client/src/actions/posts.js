664da4490863e90498c74fbe804f7e86e9ed0100
Pasindu-Lakmal <pasindu1llak@gmail.com>
2022-09-11 12:26:14 +0530

    form

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -10,3 +10,10 @@ export const getPost = () => async (dispatch) => {
     console.log(error.message);
   }
 };
+
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+    dispatch({ type: "CREATE", paylode: data });
+  } catch (error) {}
+};
